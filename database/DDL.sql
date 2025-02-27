d3be09fda4338894aca2d1f8d3795ad05cbd69b2
Santiago Romero <srp.devel@gmail.com>
2025-02-27 09:57:53 -0600

    Database DDL+DML skeleton

---
+++ database/DDL.sql
@@ -0,0 +1,94 @@
+/*
+Script to create the database schema.
+*/
+
+DROP SCHEMA IF EXISTS public CASCADE;
+CREATE SCHEMA public;
+
+--------------------------------------------------------
+-------------------- TABLE CREATION --------------------
+--------------------------------------------------------
+
+-- Accompanies table
+-- Armor table
+-- Assignment table
+-- Befriends table
+-- Character table
+-- Clan table
+-- Class table
+-- Equips table
+-- Invites table
+-- OwnsArmor table
+-- OwnsPet table
+-- OwnsWeapon table
+-- Pet table
+-- Tags table
+-- User table
+-- Weapon table
+-- Wears table
+
+--------------------------------------------------------
+------------------ ENTITY RESTRICTIONS -----------------
+--------------------------------------------------------
+
+-- Accompanies table
+-- Armor table
+-- Assignment table
+-- Befriends table
+-- Character table
+-- Clan table
+-- Class table
+-- Equips table
+-- Invites table
+-- OwnsArmor table
+-- OwnsPet table
+-- OwnsWeapon table
+-- Pet table
+-- Tags table
+-- User table
+-- Weapon table
+-- Wears table
+
+--------------------------------------------------------
+------------------ DOMAIN RESTRICTIONS -----------------
+--------------------------------------------------------
+
+-- Accompanies table
+-- Armor table
+-- Assignment table
+-- Befriends table
+-- Character table
+-- Clan table
+-- Class table
+-- Equips table
+-- Invites table
+-- OwnsArmor table
+-- OwnsPet table
+-- OwnsWeapon table
+-- Pet table
+-- Tags table
+-- User table
+-- Weapon table
+-- Wears table
+
+--------------------------------------------------------
+--------------- REFERENTIAL RESTRICTIONS ---------------
+--------------------------------------------------------
+
+-- Accompanies table
+-- Armor table
+-- Assignment table
+-- Befriends table
+-- Character table
+-- Clan table
+-- Class table
+-- Equips table
+-- Invites table
+-- OwnsArmor table
+-- OwnsPet table
+-- OwnsWeapon table
+-- Pet table
+-- Tags table
+-- User table
+-- Weapon table
+-- Wears table
---
+++ database/DML.sql
@@ -0,0 +1,21 @@
+/*
+Script to populate the database with mock data.
+*/
+
+-- Accompanies table
+-- Armor table
+-- Assignment table
+-- Befriends table
+-- Character table
+-- Clan table
+-- Class table
+-- Equips table
+-- Invites table
+-- OwnsArmor table
+-- OwnsPet table
+-- OwnsWeapon table
+-- Pet table
+-- Tags table
+-- User table
+-- Weapon table
+-- Wears table
