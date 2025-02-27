09550aea69df2d18330af88c9a59ee0ad5d4d705
icmor <icornejomora@gmail.com>
2025-02-26 22:12:53 -0600

    Docker
    Add a basic docker file and docker-compose.yml

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main () {
+	port := os.Getenv("PORT")
+	if port == "" {
+		fmt.Println("Port undefined, exiting app...")
+		os.Exit(0)
+	}
 	app := fiber.New()
 	app.Get("/", func(ctx *fiber.Ctx) error {
 		err := ctx.SendString("Hello fiber!")
 		return err
 	})
-	app.Listen(":8080")
+	app.Listen(":" + port)
 }
