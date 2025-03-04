07deb6e088aa8e5ea4a2ddb49813984cb22adc95
Vidal Aguilar Diego Jesus <vidalaguilardiego@ciencias.unam.mx>
2025-03-03 22:48:00 -0600

    Hello World with Air and init on up

--- Backend/main.go
+++ Backend/main.go
@@ -9,7 +9,7 @@ func main() {
     app := fiber.New()
 
     app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World!")
+        return c.SendString("Hello, Estudiantica")
     })
 
     log.Fatal(app.Listen(":3000"))
