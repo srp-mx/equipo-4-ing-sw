7e1383c43b09a725df2a5e54f9dbd1521d1a3697
Santiago Romero <srp.devel@gmail.com>
2025-03-06 16:23:12 -0600

    Changes to testing framework

--- Backend/test/example_test.go
+++ Backend/test/example_test.go
@@ -1,13 +1,14 @@
-package test
+package main
 
 import (
-	"math"
 	"testing"
+	"math"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAbs(t *testing.T) {
-    got := math.Abs(-1)
-    if got != 1 {
-        t.Errorf("Abs(-1) = %d; want 1", got)
-    }
+    t.Run("absolute value test", func(t *testing.T) {
+        got := math.Abs(-1.0)
+        assert.Equal(t, 1.0, got)
+    })
 }
