41fbce20f33135b0ddb4ae3a775cc7ac52dc6358
Vidal Aguilar Diego Jesus <vidalaguilardiego@ciencias.unam.mx>
2025-03-07 04:53:02 -0600

    Change in Test User Controller and User Controller

--- Backend/test/UserController_test.go
+++ Backend/test/UserController_test.go
@@ -2,103 +2,93 @@ package test
 
 import (
 	"testing"
-	"github.com/srp-mx/equipo-4-ing-sw/Controllers/UserController.go"
-	"github.com/srp-mx/equipo-4-ing-sw/models/User.go"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	
+	"github.com/srp-mx/equipo-4-ing-sw/models"
+	"github.com/srp-mx/equipo-4-ing-sw/Controllers"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 )
 
-type MockUserController struct {
-	mock.Mock
-}
-
-func (m *MockUserController) CreateUser(user *models.User) error {
-	args := m.Called(user)
-	return args.Error(0)
-}
-
-func (m *MockUserController) UpdateUser(user *models.User) error {
-	args := m.Called(user)
-	return args.Error(0)
-}
-
-func (m *MockUserController) DeleteUser(user *models.User) error {
-	args := m.Called(user)
-	return args.Error(0)
-}
-
-func (m *MockUserController) FindByCredentials (email string, password string) (* models.User, error) {
-	args := m.Called(email, password)
-	return args.Get(0).(*modelsUser), args.Error(1)
+func setupTestDB() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&models.User{})
+	return db
 }
 
 func TestCreateUser(t *testing.T) {
-	mockRepo := new(MockUserController)
-	userController := UserController.NewUserController(mockRepo)
+	db := setupTestDB()
+	controller := Controllers.NewUserController(db)
 
 	user := &models.User{
-		Username: "pedrito",
-		Name: "Pedrote",
-		Email: "Pedrito@piedras.com",
-		Password: "piedras",
+		Username: "testuser",
+		Password: "securepassword",
 	}
 
-	mockRepo.On("CreateUser", user).Return(nil)
-	err := userController.CreateUser(user)
-	assert.NoError(t,err)
-	mockRepo.AssertCalled(t, "CreateUser", user)
+	err := controller.CreateUser(user)
+	assert.NoError(t, err)
+
+	var foundUser models.User
+	db.First(&foundUser, "username = ?", "testuser")
+
+	assert.Equal(t, user.Username, foundUser.Username)
 }
 
 func TestUpdateUser(t *testing.T) {
-	mockRepo := new(MockUserController)
-	userController := UserController.NewUserController(mockRepo)
+	db := setupTestDB()
+	controller := Controllers.NewUserController(db)
 
 	user := &models.User{
-		Username: "pedrito",
-		Name: "Pedrote",
-		Email: "Pedrito@piedras.com",
-		Password: "piedras",
+		Username: "testuser",
+		Password: "oldpassword",
 	}
+	db.Create(user)
 
-	mockRepo.On("UpdateUser", user).Return(nil)
-	err := userController.UpdateUser(user)
-	assert.NoError(t,err)
-	mockRepo.AssertCalled(t, "UpdateUser", user)
+	user.Password = "newpassword"
+	err := controller.UpdateUser(user)
+	assert.NoError(t, err)
+
+	var updatedUser models.User
+	db.First(&updatedUser, "username = ?", "testuser")
+
+	assert.Equal(t, "newpassword", updatedUser.Password)
 }
 
-func DeleteUser(t *testing.T) {
-	mockRepo := new(MockUserController)
-	userController := UserController
+func TestDeleteUser(t *testing.T) {
+	db := setupTestDB()
+	controller := Controllers.NewUserController(db)
 
 	user := &models.User{
-		username: "pedrito",
-		name: "Pedrote",
-		Email: "Pedrito@piedras.com",
-		Password: "piedras",
+		Username: "testuser",
+		Password: "password",
 	}
-	
-	mockRepo.On("DeleteUser", user).Return(nil)
-	err := userController.DeleteUser(user)
-	assert.NoError(t,err)
-	mockRepo.AssertCalled(t, "DeleteUser", user)
+	db.Create(user)
+
+	err := controller.DeleteUser(user)
+	assert.NoError(t, err)
+
+	var foundUser models.User
+	result := db.First(&foundUser, "username = ?", "testuser")
+
+	assert.Error(t, result.Error) 
 }
 
 func TestFindByCredentials(t *testing.T) {
-	mockRepo := new(MockUserController)
-	userController := UserController.NewUserController(mockRepo)
-	
+	db := setupTestDB()
+	controller := Controllers.NewUserController(db)
+
 	user := &models.User{
-		Username: "pedrito",
-		Name: "Pedrote",
-		Email: "Pedrito@piedras.com",
-		Password: "piedras",
+		Username: "testuser",
+		Password: "securepassword",
 	}
+	db.Create(user)
 
-	mockRepo.On("FidnByCredentials", "pedrito", "piedras").Return(user, nil)
-	ruser, err := userController.FindByCredentials("pedrito", "piedras")
+	foundUser, err := controller.FindByCredentials("testuser", "securepassword")
 	assert.NoError(t, err)
-	assert.Equal(t,user,ruser)
-	mockRepo.AssertCalled(t,"FindBycredentials", "pedrito", "piedras")
+	assert.NotNil(t, foundUser)
+	assert.Equal(t, "testuser", foundUser.Username)
 }
-
-
