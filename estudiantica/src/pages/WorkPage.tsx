3a076e80a982bf4e9804108a74739647c088c6a3
Vidal Aguilar Diego Jesus <vidalaguilardiego@ciencias.unam.mx>
2025-03-21 16:49:46 -0600

    Base

--- estudiantica/src/pages/WorkPage.tsx
+++ estudiantica/src/pages/WorkPage.tsx
@@ -1,12 +1,13 @@
 import Header from "@/components/Header";
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/home/Navbar";
 import SearchbarWorks from "@/components/SearchbarWorks";
+import SideBar from "@/components/SideBar";
 
 export default function WorkPage(){
     return (
         <div className="bg-[#0B090F] h-screen w-screen bg-cover bg-center">
-            <Header/>
             <Navbar/>
+            <SideBar/>
             <div className="p-4 sm:ml-56 bg-[url(/assets/img/login_bg.jpg)] sm:mr-4 rounded-2xl h-17/20">
                 <h1 className="title-section text-[60px]">
                     Tareas
