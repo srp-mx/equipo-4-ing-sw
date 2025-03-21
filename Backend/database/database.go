b8b6cbf33271393a40886b11313e841e42013a19
Vidal Aguilar Diego Jesus <vidalaguilardiego@ciencias.unam.mx>
2025-03-21 16:01:49 -0600

    DataBase elements testing

--- Backend/database/database.go
+++ Backend/database/database.go
@@ -8,6 +8,7 @@ import (
 	"gorm.io/gorm/logger"
 	"log"
 	"os"
+	"time"
 )
 
 type Dbinstance struct {
@@ -41,10 +42,55 @@ func ConnectDb() {
 	db.AutoMigrate(&models.User{})
 	db.AutoMigrate(&models.Class{})
 
+
 	UsuariosBase(db)
+	ClassPrueba(db)
+	AssigmentsPrueba(db)
 	DB = Dbinstance{
 		Db: db,
 	}
+}
+
+func ClassPrueba(db *gorm.DB) {
+	clases := []models.Class{
+		{ ID: 1, Name: "Informatica", StartDate: time.Now() , EndDate: time.Now() , OwnerUsername: "marinela", GradeFormula: "average(Homework)", Color: "000000"},
+	}
+	for _,clase := range clases {
+		var existing models.Class
+		err := db.Where("id = ? AND  owner_username  = ?", clase.ID, clase.OwnerUsername).First(&existing).Error
+		if err != nil {
+			if err == gorm.ErrRecordNotFound {
+				db.Create(&clase)
+				fmt.Printf("Clase creada: %s\n", clase.Name)
+			} else {
+				log.Fatalf("Error al verificar Clase: %v", err)
+			}
+		} else {
+			fmt.Printf("La clase ya existe: %s\n", clase.Name)
+		}
+	} 
+}
+
+func AssigmentsPrueba(db *gorm.DB) {
+	ass := []models.Assignment{
+		{ ID: 1, ClassID: 1,Name: "Informar", DueDate: time.Now(), Tag: "Homework"},
+		{ ID: 2, ClassID: 1,Name: "Tejer", DueDate: time.Now(), Tag: "Homework"},
+		{ ID: 3, ClassID: 1,Name: "Hackear la Nasa", DueDate: time.Now(), Tag: "Homework"},
+	}
+	for _,a := range ass {
+		var existing models.Assignment
+		err := db.Where("id = ? AND  class_id  = ?", a.ID, a.ClassID).First(&existing).Error
+		if err != nil {
+			if err == gorm.ErrRecordNotFound {
+				db.Create(&a)
+				fmt.Printf("Tarea creada: %s\n", a.Name)
+			} else {
+				log.Fatalf("Error al verificar Tarea: %v", err)
+			}
+		} else {
+			fmt.Printf("La tarea ya existe: %s\n", a.Name)
+		}
+	} 
 }
 
 func UsuariosBase(db *gorm.DB) {
