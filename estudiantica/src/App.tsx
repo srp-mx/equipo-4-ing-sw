872c7c633d94d284a9d81d60d015a9778d1e9bc9
Vidal Aguilar Diego Jesus <vidalaguilardiego@ciencias.unam.mx>
2025-03-21 16:04:00 -0600

    Rebase

--- estudiantica/src/App.tsx
+++ estudiantica/src/App.tsx
@@ -2,10 +2,21 @@ import React, { useEffect, useState } from 'react';
 import Login from './pages/Login';
 import Inicio from './pages/Inicio';
 import WorkPage from './pages/WorkPage';
+<<<<<<< HEAD
 import ClassPage from './pages/ClassPage';
 
 export default function App() {
   return <Login/>
 }
+=======
+import LandingPage from "./pages/LandingPage/LandingPage";
+import Home from "./pages/Home";
+const App = () => {
+  return (
+    //<LandingPage/>
+    <Home />
+  );
+};
+>>>>>>> 1a6a445 (Home Page without Character View, need to implemented Character Component view to finish the Home Page)
 
 export default App;
