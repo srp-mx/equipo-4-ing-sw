c2d2c0e13c689d2ebf4fef2e5549136114f513b0
Vidal Aguilar Diego Jesus <vidalaguilardiego@ciencias.unam.mx>
2025-03-20 20:44:40 -0600

    rebase aplicado

--- estudiantica/src/App.tsx
+++ estudiantica/src/App.tsx
@@ -2,46 +2,12 @@ import React, { useEffect, useState } from 'react';
 import Login from './pages/Login';
 import Inicio from './pages/Inicio';
 import WorkPage from './pages/WorkPage';
-/*import LandingPage from './pages/LandingPage';
-const url = process.env.url || 'http://localhost:3000'
+import LandingPage from "./pages/LandingPage/LandingPage";
 
-async function validateToken(token : string) : Promise<boolean>{
-    try{
-        const response = await fetch(`${url}/landing`,{
-            method : 'GET',
-            headers : {
-                'Authorization' : `Bearer ${token}`
-            }
-        })
+const App = () => {
+  return (
+    <LandingPage/>
+  );
+};
 
-        return response.status === 200;
-    }catch(error){
-        console.log(`Error al verificar el token:`,error);
-        return false;
-    }
-}*/
-
-export default function App() {
-  /*const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
-
-  useEffect(() => {
-    const token = localStorage.getItem('Token');
-
-    if (!token) {
-      setIsAuthenticated(false);
-      return;
-    }
-
-    validateToken(token).then(isValid => {
-      setIsAuthenticated(isValid);
-    });
-  }, []);
-
-  if (isAuthenticated === null) {
-    return <div className="flex h-screen items-center justify-center">Cargando...</div>;
-  }
-
-  return isAuthenticated ? <LandingPage /> : <Login />;
-  return <Login/>;*/
-  return <Inicio/>
-}
+export default App;
--- estudiantica/src/components/Navbar.tsx
+++ estudiantica/src/components/Navbar.tsx
@@ -1,30 +1,68 @@
-export default function Navbar(){
+import {Menu, X} from "lucide-react";
+import { useState } from "react";
+import logo from "../assets/Logo.png";
+import '../index.css'
+import { navItems } from "../constants";
+
+const NavBar = () => {
+    const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+    
+    const toggleNavBar = () => {
+        setMobileDrawerOpen(!mobileDrawerOpen);
+    };
     return (
-        <aside className="fixed rounded-xl ml-4 fixed left-0 z-40">
-        <div className="h-full px-3 py-4 overflow-y-auto bg-[#1A1538] rounded-2xl">
-            <ul className="space-y-8 text-white">
-                <a href="Inicio" className="flex items-center p-2 rounded-lg transition hover:-translate-y-1 hover:scale-100 group">
-                    <img src="assets/img/sidebar/icono_inicio.png" className="mr-3 duration-75"/>
-                    Inicio
-                </a>
-                <a href="#" className="flex items-center p-2 rounded-lg transition hover:-translate-y-1 hover:scale-100 group">
-                    <img src="assets/img/sidebar/icono_clanes.png" className="mr-3"/>
-                    Clanes
-                </a>
-                <a href="#" className="flex items-center p-2 rounded-lg transition hover:-translate-y-1 hover:scale-100 group">
-                    <img src="assets/img/sidebar/icono_mazmorras.png" className="mr-3"/>
-                    Mazmorras
-                </a>
-                <a href="#" className="flex items-center p-2 rounded-lg transition hover:-translate-y-1 hover:scale-100 group">
-                    <img src="assets/img/sidebar/icono_otros.png" className="mr-3"/>
-                    Otros
-                </a>
-                <a href="#" className="flex items-center p-2 rounded-lg transition hover:-translate-y-1 hover:scale-100 group">
-                    <img src="assets/img/sidebar/icono_configuración.png" className="mr-3"/>
-                    Configuración
-                </a>    
-            </ul>
-        </div>
-    </aside>
+        <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b
+        border-neutral-700 opacity-80">
+            <div className="container px-4 mx-auto relative text-sm">
+                <div className="flex justify-between items-center">
+                    <div className="flex items-center flex-shrink-0">
+                        <img className="h-10 w-10 mr-2" src={logo} alt="logo" />
+                        <span className="text-xl tracking-tight">Estudiantica</span>
+                    </div>
+                    <ul className="hidden lg:flex ml-14 space-x-12">
+                        {navItems.map((item, index) => (
+                            <li key={index}>
+                                <a href={item.href}>{item.label}</a>
+                            </li>
+                        ))}
+                    </ul>
+                    <div className="hidden lg:flex justify-center space-x-12 items-center">
+                        <a href="#" className="py-2 px-3 border rounded-md">
+                            Sign up
+                        </a>
+                        <a href="#" className="bg-gradient-to-r from-blue-500 to-blue-800 py-2 px-3 rounded-md">
+                            Log in
+                        </a>
+                    </div>
+                    <div className="lg:hidden md:flex flex-col justify-end">
+                        <button onClick={toggleNavBar}> 
+                            {mobileDrawerOpen ? <X /> : <Menu /> }
+                        </button>
+                    </div>
+                </div>
+                {mobileDrawerOpen && (
+                    <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+                        <ul>
+                        {navItems.map((item,index) => (
+                            <li key={index} className="py-4">
+                                <a href={item.href}>{item.label}</a>
+                            </li>
+                        ))}
+                        </ul>
+                        <div className="flex space-x-6">
+                            <a href="#" className="py-2 px-3 border rounded-md">
+                                Sign up
+                            </a>
+                            <a href="#" className="py-2 px-3 rounded-md bg-gradient-to-r from-blue-500 to-blue-800">
+                                Log in
+                            </a>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </nav>
     );
 }
+
+export default NavBar;
+>>>>>>> cde82e1 (Landing Page creation html and css in a simple form, without links to others pages and register or login)
