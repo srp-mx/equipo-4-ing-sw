8fe093af56203621a4fd3a38e746cbaaaa718e8c
Santiago Romero <srp.devel@gmail.com>
2025-03-20 22:54:13 -0600

    Used go fmt on older backend files

--- Backend/controllers/FormulaTranslator.go
+++ Backend/controllers/FormulaTranslator.go
@@ -27,91 +27,91 @@ import (
 
 // Contains uninterpreted and interpreted data of a formula.
 type Formula struct {
-    Source string
-    Expression *govaluate.EvaluableExpression
-    TagsUsed []string
-    Error error
+	Source     string
+	Expression *govaluate.EvaluableExpression
+	TagsUsed   []string
+	Error      error
 }
 
 // Formula constructor
 func NewFormula(input string) (*Formula, error) {
-    expr, err := govaluate.NewEvaluableExpressionWithFunctions(input, functions)
-    if err != nil {
-        return &Formula {
-            Source: input,
-            Expression: nil,
-            TagsUsed: []string{},
-            Error: err,
-        }, err
-    }
+	expr, err := govaluate.NewEvaluableExpressionWithFunctions(input, functions)
+	if err != nil {
+		return &Formula{
+			Source:     input,
+			Expression: nil,
+			TagsUsed:   []string{},
+			Error:      err,
+		}, err
+	}
 
-    formula := Formula {
-        Source: input,
-        Expression: expr,
-        TagsUsed: []string{},
-        Error: nil,
-    }
+	formula := Formula{
+		Source:     input,
+		Expression: expr,
+		TagsUsed:   []string{},
+		Error:      nil,
+	}
 
-    formula.parseTags()
+	formula.parseTags()
 
-    return &formula, nil
+	return &formula, nil
 }
 
 // Helper method to retrieve the tags needed for a formula to evaluate.
 func (self *Formula) parseTags() {
-    tokens := self.Expression.Tokens()
+	tokens := self.Expression.Tokens()
 
-    tagMap := make(map[string]bool)
-    for _, token := range tokens {
-        if token.Kind == govaluate.VARIABLE {
-            tagMap[token.Value.(string)] = true
-        }
-    }
+	tagMap := make(map[string]bool)
+	for _, token := range tokens {
+		if token.Kind == govaluate.VARIABLE {
+			tagMap[token.Value.(string)] = true
+		}
+	}
 
-    var tags []string
-    for key := range tagMap {
-        tags = append(tags, key)
-    }
+	var tags []string
+	for key := range tagMap {
+		tags = append(tags, key)
+	}
 
-    self.TagsUsed = tags
+	self.TagsUsed = tags
 }
 
 // Given a tag-grades map, evaluates the formula.
 func (self *Formula) Evaluate(tagMap map[string]any) (result float64, err error) {
-    defer func() {
-        if catch := recover(); catch != nil {
-            result = 0.0
-            err = fmt.Errorf("La fórmula no dio un resultado válido.")
-        }
-    }()
+	defer func() {
+		if catch := recover(); catch != nil {
+			result = 0.0
+			err = fmt.Errorf("La fórmula no dio un resultado válido.")
+		}
+	}()
 
-    if !self.allTagsFilled(tagMap) {
-        return 0.0, fmt.Errorf("Hay tags en la fórmula que aún no existen.")
-    }
+	if !self.allTagsFilled(tagMap) {
+		return 0.0, fmt.Errorf("Hay tags en la fórmula que aún no existen.")
+	}
 
-    r, err := self.Expression.Evaluate(tagMap)
-    if err != nil {
-        return 0.0, err
-    }
-    if result, ok := r.(float64); ok {
-        if math.IsNaN(result) || math.IsInf(result, 0) {
-            err = fmt.Errorf("La fórmula se indefinió o fue a infinito.")
-        }
-        return result, err
-    }
-    result = 0.0
-    err = fmt.Errorf("La fórmula debe evaluarse a un número.")
-    return result, err
+	r, err := self.Expression.Evaluate(tagMap)
+	if err != nil {
+		return 0.0, err
+	}
+	if result, ok := r.(float64); ok {
+		if math.IsNaN(result) || math.IsInf(result, 0) {
+			err = fmt.Errorf("La fórmula se indefinió o fue a infinito.")
+		}
+		return result, err
+	}
+	result = 0.0
+	err = fmt.Errorf("La fórmula debe evaluarse a un número.")
+	return result, err
 }
 
 // Helper that checks if all tags used in the formula appear on the assignments.
 func (self *Formula) allTagsFilled(tagMap map[string]any) bool {
-    for _,tag := range self.TagsUsed {
-        if _,exists := tagMap[tag]; !exists {
-            return false
-        }
-    }
-    return true
+	for _, tag := range self.TagsUsed {
+		if _, exists := tagMap[tag]; !exists {
+			return false
+		}
+	}
+	return true
 }
 
 // Verifies whether a formula is plausible, i.e. if it fails on execution.
@@ -119,205 +119,205 @@ func (self *Formula) allTagsFilled(tagMap map[string]any) bool {
 // so we can't catch all without disallowing formulae that may be valid on
 // some contexts. Thus, we should also check for errors on evaluation.
 func (self *Formula) VerifyPlausibility() (isValid bool) {
-    if self.Error != nil {
-        return false
-    }
-    testData := make(map[string]any)
-    for i,tag := range self.TagsUsed {
-        testData[tag] = []float64 { float64(i+1) }
-    }
-    defer func() {
-        if catch := recover(); catch != nil {
-            isValid = false
-            self.Error = fmt.Errorf("La fórmula no es válida.")
-        }
-    }()
-    _, err := self.Evaluate(testData)
-    self.Error = err
-    isValid = err == nil
-    return isValid
+	if self.Error != nil {
+		return false
+	}
+	testData := make(map[string]any)
+	for i, tag := range self.TagsUsed {
+		testData[tag] = []float64{float64(i + 1)}
+	}
+	defer func() {
+		if catch := recover(); catch != nil {
+			isValid = false
+			self.Error = fmt.Errorf("La fórmula no es válida.")
+		}
+	}()
+	_, err := self.Evaluate(testData)
+	self.Error = err
+	isValid = err == nil
+	return isValid
 }
 
 // The map with the custom functions formulae accept.
-var functions map[string]govaluate.ExpressionFunction = map[string]govaluate.ExpressionFunction {
-    "sum": func(args ...any) (any, error) {
-        values := toFloat64Slice(args)
-        return sum(values)
-    },
-    "average": func(args ...any) (any, error) {
-        values := toFloat64Slice(args)
-        return average(values)
-    },
-    "median": func(args ...any) (any, error) {
-        values := toFloat64Slice(args)
-        return median(values)
-    },
-    "min": func(args ...any) (any, error) {
-        values := toFloat64Slice(args)
-        return minimum(values)
-    },
-    "max": func(args ...any) (any, error) {
-        values := toFloat64Slice(args)
-        return maximum(values)
-    },
-    "top": func(args ...any) (any, error) {
-        if len(args) < 2 {
-            return []float64{},
-                fmt.Errorf("La función top debe tener al menos dos argumentos.")
-        }
-        count, ok := args[0].(float64)
-        if !ok || count != float64(int(count)) {
-            return []float64{},
-                fmt.Errorf("El primer argumento debe ser un número entero en top.")
-        }
-        values := toFloat64Slice(args[1:])
-        return takeTop(int(count), values)
-    },
-    "bottom": func(args ...any) (any, error) {
-        if len(args) < 2 {
-            return []float64{},
-                fmt.Errorf("La función bottom debe tener al menos dos argumentos.")
-        }
-        count, ok := args[0].(float64)
-        if !ok || count != float64(int(count)) {
-            return []float64{},
-                fmt.Errorf("El primer argumento debe ser un número entero en bottom.")
-        }
-        values := toFloat64Slice(args[1:])
-        return takeBottom(int(count), values)
-    },
-    "offset": func(args ...any) (any, error) {
-        if len(args) < 2 {
-            return []float64{},
-                fmt.Errorf("La función offset debe tener al menos dos argumentos.")
-        }
-        off, ok := args[0].(float64)
-        if !ok {
-            return []float64{},
-                fmt.Errorf("El primer argumento debe ser un número en offset.")
-        }
-        values := toFloat64Slice(args[1:])
-        return offset(off, values)
-    },
-    "scale": func(args ...any) (any, error) {
-        if len(args) < 2 {
-            return []float64{},
-                fmt.Errorf("La función scale debe tener al menos dos argumentos.")
-        }
-        sc, ok := args[0].(float64)
-        if !ok {
-            return []float64{},
-                fmt.Errorf("El primer argumento debe ser un número en scale.")
-        }
-        values := toFloat64Slice(args[1:])
-        return scale(sc, values)
-    },
+var functions map[string]govaluate.ExpressionFunction = map[string]govaluate.ExpressionFunction{
+	"sum": func(args ...any) (any, error) {
+		values := toFloat64Slice(args)
+		return sum(values)
+	},
+	"average": func(args ...any) (any, error) {
+		values := toFloat64Slice(args)
+		return average(values)
+	},
+	"median": func(args ...any) (any, error) {
+		values := toFloat64Slice(args)
+		return median(values)
+	},
+	"min": func(args ...any) (any, error) {
+		values := toFloat64Slice(args)
+		return minimum(values)
+	},
+	"max": func(args ...any) (any, error) {
+		values := toFloat64Slice(args)
+		return maximum(values)
+	},
+	"top": func(args ...any) (any, error) {
+		if len(args) < 2 {
+			return []float64{},
+				fmt.Errorf("La función top debe tener al menos dos argumentos.")
+		}
+		count, ok := args[0].(float64)
+		if !ok || count != float64(int(count)) {
+			return []float64{},
+				fmt.Errorf("El primer argumento debe ser un número entero en top.")
+		}
+		values := toFloat64Slice(args[1:])
+		return takeTop(int(count), values)
+	},
+	"bottom": func(args ...any) (any, error) {
+		if len(args) < 2 {
+			return []float64{},
+				fmt.Errorf("La función bottom debe tener al menos dos argumentos.")
+		}
+		count, ok := args[0].(float64)
+		if !ok || count != float64(int(count)) {
+			return []float64{},
+				fmt.Errorf("El primer argumento debe ser un número entero en bottom.")
+		}
+		values := toFloat64Slice(args[1:])
+		return takeBottom(int(count), values)
+	},
+	"offset": func(args ...any) (any, error) {
+		if len(args) < 2 {
+			return []float64{},
+				fmt.Errorf("La función offset debe tener al menos dos argumentos.")
+		}
+		off, ok := args[0].(float64)
+		if !ok {
+			return []float64{},
+				fmt.Errorf("El primer argumento debe ser un número en offset.")
+		}
+		values := toFloat64Slice(args[1:])
+		return offset(off, values)
+	},
+	"scale": func(args ...any) (any, error) {
+		if len(args) < 2 {
+			return []float64{},
+				fmt.Errorf("La función scale debe tener al menos dos argumentos.")
+		}
+		sc, ok := args[0].(float64)
+		if !ok {
+			return []float64{},
+				fmt.Errorf("El primer argumento debe ser un número en scale.")
+		}
+		values := toFloat64Slice(args[1:])
+		return scale(sc, values)
+	},
 }
 
 // Utility functions that do the computation for the formulae.
 
 // Adds the values up.
 func sum(values []float64) (float64, error) {
-    sum := 0.0
-    for _,v := range values {
-        sum += v
-    }
-    return sum, nil
+	sum := 0.0
+	for _, v := range values {
+		sum += v
+	}
+	return sum, nil
 }
 
 // Gets the average of the values.
 func average(values []float64) (float64, error) {
-    if len(values) == 0 {
-        return 0.0, nil
-    }
-    sum, err := sum(values)
-    return sum / float64(len(values)), err
+	if len(values) == 0 {
+		return 0.0, nil
+	}
+	sum, err := sum(values)
+	return sum / float64(len(values)), err
 }
 
 // Gets the median of the values.
 func median(values []float64) (float64, error) {
-    if len(values) == 0 {
-        return 0.0, nil
-    }
-    sort.Float64s(values)
-    median := values[(len(values))/2]
-    if len(values) % 2 == 0 {
-        median = (median + values[len(values)/2 - 1]) / 2.0
-    }
-    return median, nil
+	if len(values) == 0 {
+		return 0.0, nil
+	}
+	sort.Float64s(values)
+	median := values[(len(values))/2]
+	if len(values)%2 == 0 {
+		median = (median + values[len(values)/2-1]) / 2.0
+	}
+	return median, nil
 }
 
 // Gets the minimum of the values.
 func minimum(values []float64) (float64, error) {
-    if len(values) == 0 {
-        return 0.0, nil
-    }
-    minV := values[0]
-    for _,v := range values {
-        if v < minV {
-            minV = v
-        }
-    }
-    return minV, nil
+	if len(values) == 0 {
+		return 0.0, nil
+	}
+	minV := values[0]
+	for _, v := range values {
+		if v < minV {
+			minV = v
+		}
+	}
+	return minV, nil
 }
 
 // Gets the maximum of the values.
 func maximum(values []float64) (float64, error) {
-    if len(values) == 0 {
-        return 0.0, fmt.Errorf("Can't find maximum of zero elements.")
-    }
-    minV := values[0]
-    for _,v := range values {
-        if v > minV {
-            minV = v
-        }
-    }
-    return minV, nil
+	if len(values) == 0 {
+		return 0.0, fmt.Errorf("Can't find maximum of zero elements.")
+	}
+	minV := values[0]
+	for _, v := range values {
+		if v > minV {
+			minV = v
+		}
+	}
+	return minV, nil
 }
 
 // Takes the highest n values.
 func takeTop(count int, values []float64) ([]float64, error) {
-    if count > len(values) || count <= 0 {
-        return []float64{}, nil
-    }
-    sort.Float64s(values)
-    return values[len(values)-count:], nil
+	if count > len(values) || count <= 0 {
+		return []float64{}, nil
+	}
+	sort.Float64s(values)
+	return values[len(values)-count:], nil
 }
 
 // Takes the lowest n values.
 func takeBottom(count int, values []float64) ([]float64, error) {
-    if count > len(values) || count <= 0 {
-        return []float64{}, nil
-    }
-    sort.Float64s(values)
-    return values[:count], nil
+	if count > len(values) || count <= 0 {
+		return []float64{}, nil
+	}
+	sort.Float64s(values)
+	return values[:count], nil
 }
 
 // Adds an offset to each entry.
 func offset(displacement float64, values []float64) ([]float64, error) {
-    for i,value := range values {
-        values[i] = value + displacement
-    }
-    return values, nil
+	for i, value := range values {
+		values[i] = value + displacement
+	}
+	return values, nil
 }
 
 // Multiplies each entry by a scaling factor.
 func scale(factor float64, values []float64) ([]float64, error) {
-    for i,value := range values {
-        values[i] = value * factor
-    }
-    return values, nil
+	for i, value := range values {
+		values[i] = value * factor
+	}
+	return values, nil
 }
 
 // Casts an array into an array of floats.
 func toFloat64Slice(args []any) []float64 {
-    var result []float64
-    for _, arg := range args {
-        if list, ok := arg.([]float64); ok {
-            result = append(result, list...)
-        } else if value, ok := arg.(float64); ok {
-            result = append(result, value)
-        }
-    }
-    return result
+	var result []float64
+	for _, arg := range args {
+		if list, ok := arg.([]float64); ok {
+			result = append(result, list...)
+		} else if value, ok := arg.(float64); ok {
+			result = append(result, value)
+		}
+	}
+	return result
 }
--- Backend/handlers/login.go
+++ Backend/handlers/login.go
@@ -11,55 +11,55 @@ import (
 )
 
 func Login(c *fiber.Ctx) error {
-    type loginRequest struct {
-        Email string `json:"email"`
-        Password string `json:"password"`
-    }
+	type loginRequest struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
 
-    type loginResponse struct {
-        Token string `json:"token"`
-    }
+	type loginResponse struct {
+		Token string `json:"token"`
+	}
 
-    // Get the credentials from the request body
-    request := new(loginRequest)
-    if err := c.BodyParser(request); err != nil {
-        return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-            "error": err.Error(),
-        })
-    }
+	// Get the credentials from the request body
+	request := new(loginRequest)
+	if err := c.BodyParser(request); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
 
-    // Get the user by credentials
-    userController := controllers.NewUserController(database.DB.Db)
-    user, err := userController.FindByCredentials(request.Email, request.Password)
-    if err != nil {
-        return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-            "error": fiber.Error{
-                Code: 401,
-                Message: "El usuario o contraseña no es correcto.",
-            },
-        })
-    }
+	// Get the user by credentials
+	userController := controllers.NewUserController(database.DB.Db)
+	user, err := userController.FindByCredentials(request.Email, request.Password)
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": fiber.Error{
+				Code:    401,
+				Message: "El usuario o contraseña no es correcto.",
+			},
+		})
+	}
 
-    // Create JWT claims with user ID and expiry time
-    claims := jwt.MapClaims{
-        "username": user.Username,
-        "name": user.Name,
-        "email": user.Email,
-        "exp": time.Now().Add(time.Hour * 24).Unix(),
-    }
+	// Create JWT claims with user ID and expiry time
+	claims := jwt.MapClaims{
+		"username": user.Username,
+		"name":     user.Name,
+		"email":    user.Email,
+		"exp":      time.Now().Add(time.Hour * 24).Unix(),
+	}
 
-    // Create the token
-    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// Create the token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-    // Generate and send token
-    t, err := token.SignedString([]byte(middlewares.JwtSecret))
-    if err != nil {
-        return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-            "error": err.Error(),
-        })
-    }
+	// Generate and send token
+	t, err := token.SignedString([]byte(middlewares.JwtSecret))
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
 
-    return c.JSON(loginResponse{
-        Token: t,
-    })
+	return c.JSON(loginResponse{
+		Token: t,
+	})
 }
