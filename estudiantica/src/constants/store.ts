5fe8aa7690d815a8e9b49c9bda137857c66826ac
Vidal Aguilar Diego Jesus <vidalaguilardiego@ciencias.unam.mx>
2025-04-03 22:14:42 -0600

    Fix Redux

--- estudiantica/src/constants/store.ts
+++ estudiantica/src/constants/store.ts
@@ -1,13 +1,35 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import userReducer from './userSlice'; // Importamos el slice del usuario
+import {
+  persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, 
+  PURGE, REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'root', 
+  storage,
+  whilelist:['user'],
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    user: userReducer, // Registramos el reducer
-  },
+  reducer: persistedReducer, 
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+    }
+  })
 });
+
+export const pertsistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-export default store;
+export {store};
