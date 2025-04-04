760c0a33205e274a44c840f31c009dae23218994
MedinaPeraltaJoaquin <joaquin.medina513@ciencias.unam.mx>
2025-04-03 20:43:05 -0600

    Se unificaron los estilos y se modifico la estructura de las carpetas de frontend

--- estudiantica/src/pages/WorkPage.tsx
+++ estudiantica/src/pages/WorkPage.tsx
@@ -1,14 +1,14 @@
 import Header from "@/components/Header";
-import Navbar from "@/components/home/Navbar";
+import Navbar from "@/components/Navbar";
 import SearchbarWorks from "@/components/SearchbarWorks";
 import SideBar from "@/components/SideBar";
 
 export default function WorkPage(){
     return (
         <div className="bg-[#0B090F] h-screen w-screen bg-cover bg-center">
-            <Navbar/>
+            <Navbar isLoggedIn={true}/>
             <SideBar/>
-            <div className="p-4 sm:ml-56 bg-[url(/assets/img/login_bg.jpg)] sm:mr-4 rounded-2xl h-17/20">
+            <div className="p-4 sm:ml-56 bg-[url(assets/img/login_bg.jpg)] sm:mr-4 rounded-2xl h-17/20">
                 <h1 className="title-section text-[60px]">
                     Tareas
                 </h1>
