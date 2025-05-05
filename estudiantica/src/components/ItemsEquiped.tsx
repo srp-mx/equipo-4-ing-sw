8f9c4f0f57b847fedb8b48c85dd42f159c9b2aeb
Vidal Aguilar Diego Jesus <vidalaguilardiego@ciencias.unam.mx>
2025-05-05 13:48:26 -0600

    Modals Buttons Items Equiped done without test item select in the back

--- estudiantica/src/components/ItemsEquiped.tsx
+++ estudiantica/src/components/ItemsEquiped.tsx
@@ -1,10 +1,13 @@
 import { itemEquiped } from "@/constants";
-import { setArmor, setPet, setWeapon } from "@/constants/dataCharacterSlice";
+import { clearArmor, clearItemEquiped, clearPet, clearWeapon, setArmor, setPet, setWeapon } from "@/constants/dataCharacterSlice";
 import { Armor, Pet, Weapon } from "@/Object/Items";
 import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ModalArmor from "./Items/ModalArmor";
-
+import ModalPet from "./Items/ModalPet";
+import { RootState } from "@/constants/store";
+import { CircleOff } from "lucide-react";
+import ModalWeapon from "./Items/ModalWeapon";
 
 const getPetEquiped = async (dispatch:any) => {
     try{
@@ -26,8 +29,10 @@ const getPetEquiped = async (dispatch:any) => {
             }
         }
         const data = await response.json();
-        const pet : Pet = data.pet; 
-        dispatch(setPet(pet));
+        const pet : Pet = data.pet;
+        if(pet === null) dispatch(clearPet());
+        else
+            dispatch(setPet(pet));
     }catch(error){
         console.error(error);
     }
@@ -52,6 +57,8 @@ const getArmorEquiped = async (dispatch: any) => {
         }
         const data = await response.json(); 
         const armor : Armor = data.armor;
+        if(armor === null) dispatch(clearArmor());
+        else 
         dispatch(setArmor(armor));
         }
     }catch(error){
@@ -78,6 +85,8 @@ const getWeaponEquiped = async (dispatch:any) => {
         }
         const data = await response.json(); 
         const weapon : Weapon = data.weapon;
+        if(weapon === null) dispatch(clearWeapon());
+        else
         dispatch(setWeapon(weapon));
         }
     }catch(error){
@@ -92,6 +101,7 @@ const ItemsEquiped = () => {
     const [modalWeapon, setModalWeapon] = useState(false); 
     const [modalPet, setModalPet] = useState(false);
     const dispatch = useDispatch();
+    const itemEquiped = useSelector((state: RootState) => state.dataCharacter.itemEquiped);
     useEffect(() => {
         const getEquiped = async () => {
             await getArmorEquiped(dispatch); 
@@ -104,14 +114,36 @@ const ItemsEquiped = () => {
     return (
 >>>>>>> 3d62cd6 (Modal Armor without hover)
         <>
-        {itemEquiped.map((item) => (
-            <button className="bg-gray-900 m-2 rounded-xl h-1/2 w-6/10 items-center text-center ml-4" 
+            <button className="bg-gray-900 m-2 rounded-xl h-1/2 w-6/10 flex flex-col justify-center items-center text-center ml-4" 
             onClick={() => setModalArmor(true)}
             >
-                <img src={item.url} alt={item.label} />
+                {itemEquiped.armor.id === 0 ? 
+                    <CircleOff color="black" className="w-4/5 h-4/5" />
+                : 
+                <img src={itemEquiped.armor.image_uri} alt="armadura" />
+                }
             </button>
-        ))}
+            <button className="bg-gray-900 m-2 rounded-xl h-1/2 w-6/10 flex items-center justify-center text-center ml-4" 
+            onClick={() => setModalWeapon(true)}
+            >
+                {itemEquiped.weapon.id === 0 ? 
+                    <CircleOff color="black" className="w-4/5 h-4/5" />
+                : 
+                <img src={itemEquiped.weapon.image_uri} alt="arma" />
+                }
+            </button>
+            <button className="bg-gray-900 m-2 rounded-xl h-1/2 w-6/10 flex justify-center items-center text-center ml-4" 
+            onClick={() => setModalPet(true)}
+            >
+                {0 === 0 ? 
+                    <CircleOff color="black" className="w-4/5 h-4/5" />
+                : 
+                <img src={itemEquiped.pet.image_uri} alt="mascota" />
+                }
+            </button>
         {modalArmor && <ModalArmor onClose={() => setModalArmor(false)} />}
+        {modalPet && <ModalPet onClose={() => setModalPet(false)} />}
+        {modalWeapon && <ModalWeapon onClose={() => setModalWeapon(false)}  />}
         </>
     );*/
     return (
