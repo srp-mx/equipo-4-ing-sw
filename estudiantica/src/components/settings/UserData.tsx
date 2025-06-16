ff7798a45544fb5491cc74a56b458e259956e6a4
Medina Peralta Joaquin <111473205+MedinaPeraltaJoaquin@users.noreply.github.com>
2025-06-15 23:10:11 -0600

    delete complete avatar

--- estudiantica/src/components/settings/UserData.tsx
+++ estudiantica/src/components/settings/UserData.tsx
@@ -2,7 +2,6 @@ import { useRef,useState } from "react";
 import { Button } from "../Button";
 import { Input } from "../Inputs";
 import { PasswordInput } from "../Inputs";
-import iconouser from "@/assets/img/icono_user.svg";
 import { useNotification } from "../NotificationProvider";
 
 
@@ -45,20 +44,12 @@ export default function UserData() {
     const [email, setEmail] = useState("");
     const [newPassword, setNewPassword] = useState("");
     const [confirmPassword, setconfirmPassword] = useState("");
-    const [iconoUser, setIconoUser] = useState<string>(iconouser);
-
+    
     return (
         <div className="flex justify-center items-center ">
             <div className="shadow-lg rounded-lg p-5 w-11/12 md:w-5/6 text-white">
                 <h1 className="text-2xl md:text-3xl text-white mb-3">Cambiar información personal</h1>
                 <p>Use un correo donde pueda recibir notificaciones</p>
-                <div className="flex items-center space-x-4 mb-6 mt-2">
-                    <img
-                        src={iconoUser}
-                        alt=""
-                        className="w-15 h-15 md:w-30 md:h-30 p-2 rounded-lg bg-[#CBDA3D]"
-                    />
-                </div>
 
                 <div className="space-y-2">
                     <Input
